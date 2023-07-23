a1f963973529ff7069cefa6ce5a188168e8b9c2f
hirak.mondal <hirakm2012@gmail.com>
2023-07-23 21:18:03 +0530

    New commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Git_tut
 This folder is created for the purpose of teaching git
+
+## comments
+This is a new comment
---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    set <int>s;
+    s.insert(10);
+    s.insert(20);
+    cout<<"Enter an element";
+    int a;
+    cin>>a;
+    cout<<a;
+    return 0;
+}
